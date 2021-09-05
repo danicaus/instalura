5ca1662b9ddafbe384b09f720a378e04f46eff94
Daniela Caus <danicaus.br@gmail.com>
2021-09-05 19:52:26 -0300

    Styling text as an object

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -1,5 +1,6 @@
 import styled, { css } from "styled-components";
 import get from 'lodash/get';
+import { TextStyleVariantsMap } from "../../foundation/Text";
 
 const ButtonGhost = css`
   color: ${(props) => get(props.theme, `colors.${props.variant}.color`)};
@@ -15,8 +16,9 @@ export const Button = styled.button`
   border: 0;
   cursor: pointer;
   padding: 12px 26px;
-  font-weight: bold;
   opacity: 1;
+
+  ${TextStyleVariantsMap.smallestException}
 
   transition: opacity${( { theme }) => theme.transition};
   border-radius: ${({ theme }) => theme.borderRadius};
