a04e4c5b087df301fe68322ac10d5712d4d3506b
Daniela Caus <danicaus.br@gmail.com>
2021-09-05 23:39:48 -0300

    Adjusting menuwrapper css

--- src/components/commons/Menu/styles/MenuWrapper.js
+++ src/components/commons/Menu/styles/MenuWrapper.js
@@ -9,19 +9,17 @@ export const MenuWrapper = styled.nav`
   justify-content: space-between;
   flex-wrap: wrap;
   margin-top: 18px;
-  padding-left: 28px;
-  padding-right: 28px;
+  padding: 0 28px;
   ${breakpointsMedia({
     md: css`
       justify-content: flex-start;
-      margin-top: 32px;
-      margin-left: auto;
-      margin-right: auto;
+      margin: 32px auto 0; 
       width: 100%;
       padding: 0 16px;
       max-width: 768px;
     `,
     lg: css`
+      justify-content: space-between;
       max-width: 1160px;
     `,
     xl: css`
@@ -45,29 +43,26 @@ MenuWrapper.LeftSide = styled.div`
 `
 
 MenuWrapper.CentralSide = styled.div`
-  padding: 0;
-  margin: 0;
   order: 3;
   width: 100%;
   list-style: none;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-top: 17px;
+  margin: 17px 0 0;
   border-top: 1px solid #88989E;
   border-bottom: 1px solid #99989E;
   padding: 12px;
 
   ${breakpointsMedia({
     md: css`
-      max-width: 332px;
       justify-content: space-between;
+      max-width: 332px;
       flex: 1;
       order: initial;
       border: none;
       margin: 0;
-      padding-top: 0;
-      padding-bottom: 0;
+      padding: 0 12px;
     `,
   })}
   
@@ -99,7 +94,6 @@ MenuWrapper.RightSide = styled.div`
   padding: 0;
   margin: 0;
   display: flex;
-  flex: 1;
   order: 2;
   justify-content: flex-end;
 
