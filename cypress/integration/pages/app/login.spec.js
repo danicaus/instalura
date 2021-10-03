59ae2dfd701f8be3457d86093770cd48eceaf0c2
Daniela Caus <danicaus.br@gmail.com>
2021-10-03 20:58:25 -0300

    test(test): adding a simple cypress test to login page

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -2,15 +2,15 @@
 
 describe('/pages/app/login/', () => {
   it('preencha os campos e vá para a página /app/profile', () => {
-    //visitar a página de login
+    // visitar a página de login
     cy.visit('/app/login/');
-    //preencher o input de usuário
+    // preencher o input de usuário
     cy.get('#formCadastro input[name="usuario"]').type('danicaus');
-    //preencher o input de senha
+    // preencher o input de senha
     cy.get('#formCadastro input[name="senha"]').type('senhasegura');
-    //clicar no botão de submit
+    // clicar no botão de submit
     cy.get('#formCadastro button[type="submit"]').click();
-    //ir para '/app/profile/'
-    cy.url().should('include', '/app/profile')
-  })
-})
+    // ir para '/app/profile/'
+    cy.url().should('include', '/app/profile');
+  });
+});
