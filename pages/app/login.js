3bc2902651eafc36da5ee8f36c7217406b6cbc85
Daniela Caus <danicaus.br@gmail.com>
2021-10-03 18:19:30 -0300

    feat(page): crate login and profile pages without logics
    Add HTML and CSS of Login and profile pages. We are going to start studying cypress, so I wanted to
    save this first.

--- pages/app/login.js
+++ pages/app/login.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import ErrorPage from '../../src/components/screens/errorPages';
+import LoginScreen from '../../src/components/screens/LoginScreen';
 import websitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
 
-function LoginPage() {
-  return (
-    <ErrorPage error="underConstruction" />
-  );
-}
-
-export default websitePageHOC(LoginPage, {
+export default websitePageHOC(LoginScreen, {
   pageWrapperProps: {
     seoProps: {
       headTitle: 'Login',
     },
     menuProps: {
       display: false,
+    },
+    boxProps: {
+      backgroundImage: 'url(/images/bubbles.svg)',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'bottom right',
     },
   },
 });
